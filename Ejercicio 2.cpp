1165e9d9b05839c6bbce71b3f92ba6c99a19f4cd
AngheloCM <angheloprog@gmail.com>
2025-06-01 01:51:28 -0500

    Optimizando Ejercicio 2.cpp de estructuras repetitivas

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-	int num;
-	cout<<"Por favor, ingrese un numero: ";
+	double num;
+	cout<<"Por favor, ingrese un numero: "<<endl;
 	cin>> num;
-	while (num>0){
-		cout<<"Por favor, ingrese otro numero: ";
+	while (num>=0){
+		cout<<"Por favor, ingrese otro numero: "<<endl;
 		cin>> num;
 	}
-	cout<<"Fin de la captura"<<endl;
+	cout<<"Fin de la captura :)"<<endl;
 	return 0;
 }
