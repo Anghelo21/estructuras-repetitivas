3b27ca6e1beb9ce3ecaf4db78c05cd2b715d63be
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-03 23:03:43 -0500

    Mejoramiento de interfaz para el usuario

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main(){
 	int cantidad_multiplos;
 	int acum=0;
+	cout << "+--------------------------+" << endl;
+	cout << "|      MULTIPLOS DE 7      |" <<endl;
+	cout << "+--------------------------+" << endl<<endl;
 	cout<<"Ingrese la cantidad de multiplos de 7 que quisiera saber: "<<endl;
 	cin>> cantidad_multiplos;
-	if (cantidad_multiplos<0){
-		cout<<"Solo ingrese cantidades positivas";
+	if (cantidad_multiplos<=0){
+		cout<<"Por favor, solo ingrese cantidades positivas";
 	} else{
 	cout<<"Los "<<cantidad_multiplos<<" primeros multiplos de 7 son: "<<endl;
 	for (int i=1; i<=cantidad_multiplos; i++){
 		acum=acum+7;
-		cout<<i<<" : "<<acum<<endl;
+		cout<<i<<" -> "<<acum<<endl;
 	}
 }
 	return 0;
